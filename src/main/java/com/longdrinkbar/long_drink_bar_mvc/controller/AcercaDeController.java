23039f8e247fca5d9759b0ef5545e224438d42dc
clch2409 <cesarlch2409@gmail.com>
2023-02-03 12:33:09 -0500

    Eliminacion del arcihvo estilos.css y cambios en el navbar (color de fuente)

--- src/main/java/com/longdrinkbar/long_drink_bar_mvc/controller/AcercaDeController.java
+++ src/main/java/com/longdrinkbar/long_drink_bar_mvc/controller/AcercaDeController.java
@@ -2,10 +2,15 @@ package com.longdrinkbar.long_drink_bar_mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.longdrinkbar.long_drink_bar_mvc.model.Profesor;
 @Controller
 public class AcercaDeController {
     @RequestMapping("/acercade")
     public String acercade(Model m){
+        Profesor profe = new Profesor();
+        m.addAttribute("profesor", profe);
+
         return "acercade";
     }
 }
