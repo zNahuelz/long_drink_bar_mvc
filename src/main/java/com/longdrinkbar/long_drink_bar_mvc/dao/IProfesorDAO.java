01e470b9f0c63abb0b9c8c4041ca4e6cdceb2d58
clch2409 <cesarlch2409@gmail.com>
2023-04-12 10:48:42 -0500

    modificaciones para agregar panel de control

---
+++ src/main/java/com/longdrinkbar/long_drink_bar_mvc/dao/IProfesorDAO.java
@@ -0,0 +1,11 @@
+package com.longdrinkbar.long_drink_bar_mvc.dao;
+
+import java.util.List;
+
+import com.longdrinkbar.long_drink_bar_mvc.entity.Profesor;
+
+public interface IProfesorDAO {
+    
+    //Listar docentes.
+    public List<Profesor> listarProfesor();
+}
