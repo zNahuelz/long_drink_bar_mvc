2936d5b984e052b5f5c6fd7868d969e684ae87ee
clch2409 <cesarlch2409@gmail.com>
2023-04-15 09:41:10 -0500

    Modificado el formato de fecha para la próxima clase

--- src/main/java/com/longdrinkbar/long_drink_bar_mvc/controller/DetalleCursoController.java
+++ src/main/java/com/longdrinkbar/long_drink_bar_mvc/controller/DetalleCursoController.java
@@ -54,6 +54,7 @@ public class DetalleCursoController {
         Profesor prof = profesorDAO.buscarProfesor(profC.getId_profesor().getId());
 
         Calendar calendario = Calendar.getInstance();
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
         calendario.setTime(ins.getFecha_inicio());
         
         if (calendario.getTime() == Date.from(Instant.now())){
@@ -64,10 +65,32 @@ public class DetalleCursoController {
         mv.setViewName("detalles-curso-alumno");
         mv.addObject("curso", curso);
         mv.addObject("clases", cursoDAO.listarClases(curso.getId()));
-        mv.addObject("fecha", calendario.getTime());
+        mv.addObject("fecha", formato.format(calendario.getTime()));
         mv.addObject("profesor", prof);
         mv.addObject("nombreAlum",al.getNombre()+" "+al.getApPaterno());
         AlumnoTransporter.setAlumno(al);
+        return mv;
+    }
+
+    @GetMapping(value="/detalleCursoP/{id}")
+    ModelAndView mostrarDetallesP(@PathVariable(value="id") String id, Map<String, Object> model){
+        
+        Alumno al = AlumnoTransporter.getAlumno();
+        Curso curso = cursoDAO.obtenerCurso(Integer.parseInt(id));
+        ProfesorCurso profC= cursoDAO.obtenProfesor(curso.getId());
+        Profesor prof = profesorDAO.buscarProfesor(profC.getId_profesor().getId());
+
+        //System.out.println(curso.getNombre());
+
+        ModelAndView mv = new ModelAndView(new RedirectView("detalles-curso-alumno"));
+        mv.setViewName("detalles-curso-alumno");
+        mv.addObject("curso", curso);
+        mv.addObject("turnos", cursoDAO.listarTurnos(curso.getId()));
+        mv.addObject("clases", cursoDAO.listarClases(curso.getId()));
+        mv.addObject("profesor", prof);
+        mv.addObject("nombreAlum",al.getNombre()+" "+al.getApPaterno());
+        AlumnoTransporter.setAlumno(al);
+
         return mv;
     }
 }
