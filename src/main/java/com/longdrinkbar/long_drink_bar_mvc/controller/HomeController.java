528268b47678f01edd36294392a56eea999bdd1d
clch2409 <cesarlch2409@gmail.com>
2023-04-16 12:29:48 -0500

    Terminando de implementar la interfaz de inicio con estilos y modificacin en la vista Nosotros

--- src/main/java/com/longdrinkbar/long_drink_bar_mvc/controller/HomeController.java
+++ src/main/java/com/longdrinkbar/long_drink_bar_mvc/controller/HomeController.java
@@ -1,11 +1,25 @@
 package com.longdrinkbar.long_drink_bar_mvc.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.longdrinkbar.long_drink_bar_mvc.dao.IProfesorDAO;
+import com.longdrinkbar.long_drink_bar_mvc.entity.Profesor;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    IProfesorDAO profesorDAO;
+
     @RequestMapping("/")
     public String home(Model m){
+        List<Profesor> profesores = profesorDAO.listarProfesor();
+
+        m.addAttribute("profesores", profesores);
         return "home";
     }
 }
